6b17d913018562e654994ed57c994873ef08e6e9
Marcel Koopman <marcel@MacBook-Pro-van-Esther.local>
2017-02-11 16:08:43 +0100

    FlexBudget.sol tweede versie

--- contracts/FlexBudget.sol
+++ contracts/FlexBudget.sol
@@ -1,59 +1,39 @@
 pragma solidity ^0.4.8;
 
-contract AflosbaarProduct {
-  uint balance;
-
-  function losAf(uint amount) {
-     if (balance > amount) throw;
-     balance -= amount;
-  }
-}
-
-contract MortgageContract is AflosbaarProduct {}
-contract SolarContract is AflosbaarProduct {}
-
-contract Supplier {
-  // Bank of leverancier
-
-  address owner;
-
-  function Supplier() {
-     owner = msg.sender();
-  }
-}
-
-contract SupplierForSolarContract is Supplier {}
-contract SupplierForMortgagesContract is Supplier {}
-
 contract FlexBudgetContract {
 
-    address owner; // Deelnemer
-    uint balance; // Totaal balans waarmee aflossingen kunnen worden gedaan voor deze deelnemer
-    mapping(address => AflosbaarProduct) public producten; // Aflosbare producten van toepassing voor deze deelnemer
+    address owner; // Pensioenfonds
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     function FlexBudgetContract() {
         owner = msg.sender;
     }
 
-    function losAf(address aflosbaarProduct, uint amount) {
-      if (msg.sender() != owner) {
-        throw; // onbekende deelnemer
-      }
-      AflosbaarProduct product = producten[aflosbaarProduct];
-      if (product) {
-          product.losAf(amount);
-          balance -= amount;
-      } else {
-          throw; // Product niet gevonden
-        }
-   }
+    function deposit(address deelnemer, uint amount) {
+      // Pre / postcondities
 
-    function getBalance() constant returns (uint) {
-        return balance;
+      Transfer(owner, deelnemer, amount);
     }
 
-    function getAflosbareProducten() contract returns () {
-       return producten;
-    }
 }
 
+contract PurchaseContract {
+
+    address owner; // Deelnemer
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    function PurchaseContract() {
+        owner = msg.sender;
+    }
+
+    function buyProduct(address supplier, uint price) {
+          // Pre / postcondities
+          // product
+
+          Transfer(owner, supplier, price);
+    }
+
+
+}
