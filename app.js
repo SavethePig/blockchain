6cce255bf9f08144591d13eb855be15aec73defa
Terence van Zoelen <t.van.zoelen@gmail.com>
2017-02-11 20:03:57 +0100

    clean up a bit

--- app.js
+++ app.js
@@ -20,13 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', index);
-// app.use('/users', users);
 
 app.use('/contracts', contracts);
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
